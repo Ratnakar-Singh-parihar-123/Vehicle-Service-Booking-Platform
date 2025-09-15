d639ce344145df55d8fd0731519adedc181d4a55
Ratnakar Singh <sratnakar833@gmail.com>
2025-09-15 13:47:49 +0530

    error solve

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ import toast from 'react-hot-toast';
 
 // Create axios instance
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "localhost:3000",
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
